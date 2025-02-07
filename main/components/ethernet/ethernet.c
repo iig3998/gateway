857dd3de5551e6a62d64c24380f35b72b2749d60
Fabio Molon <fabio.molon@athonet.com>
2025-02-07 09:25:12 +0100

    Fixes clock for PHY LAN87xx

--- main/components/ethernet/ethernet.c
+++ main/components/ethernet/ethernet.c
@@ -1,41 +1,57 @@
-#include "esp_err.h"
 #include "esp_event.h"
-#include "esp_eth.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
 #include "esp_netif.h"
 #include "esp_eth.h"
 
 #include "driver/gpio.h"
+
+#include "lwip/inet.h"
+
+#include "lwip/sockets.h"
+#include "sdkconfig.h"
+
+#define SOCKET_PORT         5000
+#define SOCKET_MAX_LENGTH   128
+
 #define ETH_MDC_GPIO     23
 #define ETH_MDIO_GPIO    18
 #define ETH_PHY_RST_GPIO 5
 #define ETH_PHY_ADDR     1
 #define ETH_POWER_PIN    16
 
+
 #define TAG_ETHERNET "ETHERNET"
 
 static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
 
+{
+    uint8_t mac_addr[6] = {0};
+    /* we can get the ethernet driver handle from event data */
+    esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
 
-    switch(event_id) {
-        case ETHERNET_EVENT_START:
-            ESP_LOGI(TAG_ETHERNET, "Ethernet started");
+    switch (event_id) {
+    case ETHERNET_EVENT_CONNECTED:
+        esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
+        ESP_LOGI(TAG_ETHERNET, "Ethernet Link Up");
+        ESP_LOGI(TAG_ETHERNET, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
+                    mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
         break;
-        case ETHERNET_EVENT_STOP:
-            ESP_LOGI(TAG_ETHERNET, "Ethernet stopped");
+    case ETHERNET_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG_ETHERNET, "Ethernet Link Down");
         break;
-        case ETHERNET_EVENT_CONNECTED:
-            ESP_LOGI(TAG_ETHERNET, "Ethernet connected");
+    case ETHERNET_EVENT_START:
+        ESP_LOGI(TAG_ETHERNET, "Ethernet Started");
         break;
-        case ETHERNET_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG_ETHERNET, "Ethernet disconnected");
+    case ETHERNET_EVENT_STOP:
+        ESP_LOGI(TAG_ETHERNET, "Ethernet Stopped");
+        break;
+    default:
         break;
     }
-
-    return;
+}
 }
 
+/* Event IP function */
 static void got_ip_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
 
     ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
@@ -45,21 +61,17 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base, int32_t
     return;
 }
 
-void init_eth() {
+/* Init ethernet */
+esp_err_t init_eth() {
 
     esp_err_t err = ESP_FAIL;
 
-    err = esp_netif_init();
-    if(err != ESP_OK) {
-        ESP_LOGE(TAG_ETHERNET, "Error, network interface not init");
-        return;
-    }
+    ESP_LOGI(TAG_ETHERNET, "Start Ethernet");
 
     /* Power on device */
     gpio_set_direction(ETH_POWER_PIN, GPIO_MODE_OUTPUT);
     gpio_set_level(ETH_POWER_PIN, 1);
     vTaskDelay(pdMS_TO_TICKS(100));
-    esp_event_loop_create_default();
 
     esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
     esp_netif_t *eth_netif = esp_netif_new(&cfg);
@@ -69,51 +81,56 @@ void init_eth() {
 
     esp32_emac_config.smi_gpio.mdc_num = ETH_MDC_GPIO;
     esp32_emac_config.smi_gpio.mdio_num = ETH_MDIO_GPIO;
+    esp32_emac_config.clock_config.rmii.clock_mode = EMAC_CLK_EXT_IN;
+    esp32_emac_config.clock_config.rmii.clock_gpio = EMAC_CLK_IN_GPIO;
 
     /* Create MAC instance */
     esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
     if(!mac) {
         ESP_LOGE(TAG_ETHERNET, "Error, MAC instance not created");
-        return;
+        return err;
     }
 
-    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();      // apply default PHY configuration
-    phy_config.phy_addr = ETH_PHY_ADDR;                          // alter the PHY address according to your board design
-    phy_config.reset_gpio_num = ETH_PHY_RST_GPIO;                // alter the GPIO used for PHY reset
+    /* Configure PHY ethernet */
+    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+    phy_config.phy_addr = ETH_PHY_ADDR;
+    phy_config.reset_gpio_num = ETH_PHY_RST_GPIO;
+    phy_config.autonego_timeout_ms = 1000;
 
     /* Init LAN87xx */
     esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
     if(!phy) {
         ESP_LOGE(TAG_ETHERNET, "Error, LAN87xx not init");
-        return;
+        return err;
     }
 
     esp_eth_handle_t eth_handle = NULL;
     esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
-    
+
+    /* Install driver */
     err = esp_eth_driver_install(&config, &eth_handle);
     if(err != ESP_OK) {
         ESP_LOGE(TAG_ETHERNET, "Error, ethernet driver not installed");
-        return;
+        return err;
     }
 
     /* Attach network interface */
     err = esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle));
     if (err != ESP_OK) {
-        ESP_LOGE(TAG_ETHERNET, "Error. ethernet interface not installed");
-        return;
-    }
-
-    /* Run ethernet */
-    err = esp_eth_start(eth_handle);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG_ETHERNET, "Error, ethernet not started");
-        return;
+        ESP_LOGE(TAG_ETHERNET, "Error. Ethernet interface not installed");
+        return err;
     }
 
     ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
 
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
 
-    return;
+    /* Start ethernet */
+    err = esp_eth_start(eth_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG_ETHERNET, "Error, ethernet not started");
+        return err;
+    }
+
+    return err;
 }
