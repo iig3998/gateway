880bfa6687a0b4bd14fbc250f26186bd1841fa07
Fabio Molon <fabiomolon85@gmail.com>
2025-09-03 18:42:01 +0200

    Clean conf library

--- main/components/conf/conf.h
+++ main/components/conf/conf.h
@@ -4,16 +4,6 @@
 #include <stdio.h>
 #include "esp_log.h"
 #include "esp_err.h"
-#include "esp_spiffs.h"
-#include "cJSON.h"
-#include "esp_system.h"
 
-esp_err_t init_conf();
-
-esp_err_t add_node2conf(uint8_t number_devices, uint8_t device_id, char *device_name, uint8_t *mac);
-
-esp_err_t del_node2conf(uint8_t number_devices);
-
-esp_err_t load_conf();
 
 #endif
